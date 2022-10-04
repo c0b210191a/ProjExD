3bc4d2625d714d9f322c210ed4a6a98074499580
Rimpei <c0b210191a@edu.teu.ac.jp>
2022-10-04 14:55:02 +0900

    電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,13 +3,17 @@ import tkinter.messagebox as tkm
 
 def button_click(event):
     btn = event.widget
-    txt = btn["text"]
-    tkm.showinfo(txt,f"[{num}]ボタンが押された")
+    txt = int(btn["text"])
+    #tkm.showinfo(txt,f"[{txt}]ボタンが押された")
+    entry.insert(tk.END,txt)
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("300x600")
 
-r = 0
+entry = tk.Entry(root,width=10,font=("Times New Roman",40),justify="right")
+entry.grid(row=0,column=0,columnspan=3)
+
+r = 1
 c = 0
 for i,num in enumerate(range(9,-1,-1),1):
     button = tk.Button(root,text=f"{num}",font=("Times New Roman",30),width=4,height=2)
@@ -19,7 +23,8 @@ for i,num in enumerate(range(9,-1,-1),1):
     if i%3 == 0:
         r+=1
         c = 0
-
-
+button = tk.Button(root,text="+",font=("Times New Roman",30),width=4,height=2)
+button.bind("<1>",button_click)
+button.grid(row=4,column=1)
 
 root.mainloop()
