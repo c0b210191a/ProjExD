bc29cf458690c84d4b403ba16c50eeb27b0f8deb
Rimpei <c0b210191a@edu.teu.ac.jp>
2022-10-18 14:11:54 +0900

    maze

---
+++ ex03/maze.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん") #1
+    
+    canvas = tk.Canvas(root,width=1500,height=900,bg="black")
+    canvas.pack()
+
+    root.mainloop()
