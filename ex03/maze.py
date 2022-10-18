f6afb162f7f6dcd3d5faec822cf21135015fa6ef
Rimpei <c0b210191a@edu.teu.ac.jp>
2022-10-18 17:09:21 +0900

    迷路最終版

---
+++ ex03/maze.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+import maze_maker as mm
+import random
+import tkinter.messagebox as tkm
+
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx, cy, mx, my
+    if key == "Up":
+        my -=1
+    if key =="Down":
+        my +=1
+    if key == "Left":
+        mx -= 1
+    if key == "Right":
+        mx += 1
+
+    if m_ls[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key == "Up":
+            my +=1
+        if key =="Down":
+            my -=1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
+        run_once = 0
+        tkm.showinfo("危険","進めません")
+        
+
+    canvas.coords("こうかとん",cx,cy)
+    root.after(100,main_proc)
+
+def kouka(event):
+    global fi
+    fi = f"fig/{random.randint(0,9)}.png"
+    root.after(100,kouka)
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん") #1
+    
+    canvas = tk.Canvas(root,width=1500,height=900,bg="black")
+    canvas.pack() #2
+
+    m_ls = mm.make_maze(15,9) #9
+
+    mm.show_maze(canvas,m_ls) #10
+
+    
+    fi = f"fig/{random.randint(0,9)}.png" #追加 画像ランダム
+    tori = tk.PhotoImage(file=fi)
+    mx, my =1,1
+    cx, cy = 300, 400
+    canvas.create_image(cx,cy,image=tori,tag="こうかとん") #3
+
+    key = "" #4
+
+    root.bind("<KeyPress>",key_down) #5
+
+    root.bind("<KeyRelease>",key_up) #6
+
+    
+    main_proc() #7
+
+    
+
+    root.mainloop() 
