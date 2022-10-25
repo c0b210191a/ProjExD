fda3d63369bfee50863554e2bb90fa8b029b59cd
Rimpei <c0b210191a@edu.teu.ac.jp>
2022-10-25 15:40:01 +0900

    bomb

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,9 +1,23 @@
 import pygame as pg
 import sys
+import random
+
+def check_bound(obj_rct,scr_rct):  #7  画面の外に出ないようにする関数
+    """
+    obj_rct こうかとん　または　爆弾の　rect
+    scr_rct スクリーンのrect
+    """
+    yoko, tate = 1, 1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko,tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")   #1
     scrn_sfc = pg.display.set_mode((1600,900))
+    scrn_rct = scrn_sfc.get_rect()
 
     bg_sfc = pg.image.load("fig/pg_bg.jpg")  
     bg_rct = bg_sfc.get_rect()
@@ -14,11 +28,22 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
+    #5
+    draw_sfc = pg.Surface((20,20))
+    draw_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(draw_sfc, (255, 0, 0), (10,10), 10)
+    draw_rct = draw_sfc.get_rect()
+    drawx = scrn_rct.width
+    drawy = scrn_rct.height
+    draw_rct.centerx, draw_rct.centery = random.randint(0,drawx), random.randint(0,drawy)
+
+    #6
+    vx,vy = 1, 1
+
     clock = pg.time.Clock()
     #2
     while True:  
         scrn_sfc.blit(bg_sfc, bg_rct)
-        pg.display.update()
         
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -35,9 +60,33 @@ def main():
         if key_lst[pg.K_RIGHT]:
             tori_rct.centerx += 1
 
+        #7
+        yoko, tate = check_bound(tori_rct, scrn_rct)  #符号反転
+        if yoko == -1:
+            if key_lst[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_lst[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+        if tate == -1:
+            if key_lst[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_lst[pg.K_DOWN]:
+                tori_rct.centery -= 1
+            
         scrn_sfc.blit(tori_sfc,tori_rct)
-        pg.display.update()
 
+        #7
+        yoko, tate = check_bound(draw_rct, scrn_rct)  #符号反転
+        vx *= yoko
+        vy *= tate
+        draw_rct.move_ip(vx,vy)
+        scrn_sfc.blit(draw_sfc,draw_rct)
+
+        #8
+        if tori_rct.colliderect(draw_rct):
+            return
+
+        pg.display.update()
         clock.tick(1000)
 
 
