ba3c490388b7b1942c3c754adad02764b1d13a5c
Rimpei <c0b210191a@edu.teu.ac.jp>
2022-10-25 14:19:49 +0900

    bomb

---
+++ ex04/dodge_bomb.py
@@ -0,0 +1,27 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")   #1
+    scrn_sfc = pg.display.set_mode((1600,900))
+
+    bg_sfc = pg.image.load("fig/pg_bg.jpg")  
+    bg_rct = bg_sfc.get_rect()
+
+    clock = pg.time.Clock()
+    while True:  #2
+        scrn_sfc.blit(bg_sfc, bg_rct)
+        pg.display.update()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        clock.tick(1000)
+
+
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
